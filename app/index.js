67d8b82447cf1939bc2e7f4abbda9c758483001b
Lucas Fortes <lfgfortes@gmail.com>
2026-03-22 00:36:38 -0300

    feat: adiciona tela inicial

--- app/index.js
+++ app/index.js
@@ -1,17 +1,22 @@
-import { View, Text, Button } from 'react-native';
-import { useRouter } from 'expo-router';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function Home() {
-  const router = useRouter();
-
   return (
-    <View>
-      <Text>Home</Text>
-
-      <Button
-        title="Ir para outra tela"
-        onPress={() => router.push('/itens-encontrados')}
-      />
+    <View style={styles.container}>
+      <Text style={styles.texto}>Bem-vindo ao app!</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#eb1b5b',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  texto: {
+    color: '#fff',
+    fontSize: 20,
+  },
+});
